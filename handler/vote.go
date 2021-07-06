be62f5bc04f780675dedd194df1ba9a5c1f08861
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-06 22:31:02 +0430

    vote

--- handler/vote.go
+++ handler/vote.go
@@ -1,3 +1,36 @@
 package handler
 
+import (
+	"database/sql"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+type Vote struct {
+	Store *sql.DB
+}
+
+type VoteRequest struct {
+	Score   int
+	Comment string
+}
+
+func (v *Vote) Create(c echo.Context) error {
+	user := c.Param("user")
+	film := c.Param("film")
+
+	var vote VoteRequest
+	if err := c.Bind(&vote); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	// todo user is keyword?
+	//todo what if update
+	if _, err := v.Store.Exec("INSERT INTO vote VALUES (?, ?, ?, ?)", user, film, vote.Score, vote.Comment); err != nil {
+		return err
+	}
+
+	// todo return object
+	return c.NoContent(http.StatusOK)
+}
--- migration/20210706221344_vote.up.sql
+++ migration/20210706221344_vote.up.sql
@@ -2,5 +2,9 @@ CREATE TABLE vote
 (
     user  int,
     film  int,
-    score int
+    score int,
+    comment varchar(255),
+    primary key (user, film),
+    FOREIGN KEY (user) REFERENCES registeration (id),
+    FOREIGN KEY (film) REFERENCES film (id)
 )
