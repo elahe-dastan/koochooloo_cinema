b48f1db5527c3115e299cca546fdffdafd0a7218
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 01:40:49 +0430

    feat: Add Trigger for Special User Creation

--- migration/20210710201116_favorite.up.sql
+++ migration/20210710201116_favorite.up.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS favorite
     FOREIGN KEY (film) REFERENCES film (id)
 );
 
+CREATE OR REPLACE FUNCTION special_user_validation() RETURNS trigger as
+$$
+BEGIN
+  IF (select count(*) from users where username = NEW.username and special_till > now()) = 1 THEN
+    RETURN NEW;
+  END IF;
+  RETURN NULL;
+END;
+$$
+LANGUAGE 'plpsql';
+
+CREATE TRIGGER favorite_create BEFORE INSERT on favorite FOR EACH ROW EXECUTE PROCEDURE special_user_validation();
