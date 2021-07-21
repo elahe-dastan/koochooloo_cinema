79c019b9210e40c822ab05e9dff1342a70efe844
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 01:03:01 +0430

    fix: Correct Migration Files

--- migration/20210704220245_user.up.sql
+++ migration/20210704220245_user.up.sql
@@ -1,21 +1,15 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    username        varchar(255) primary key,
-    password        varchar(255),
-    first_name       varchar(255),
-    last_name       varchar(255),
-    email           varchar(255),
-    phone           varchar(13),
-    national_number varchar(10),
-    special_till      date,
-    score           int default 0,
-    CONSTRAINT CHK_Password CHECK ( LENGTH(password) >= 8 AND  password LIKE '%[0-9]%' AND password LIKE '%[A-Z]%' AND password LIKE '%[a-z]%')
+  username        varchar(255) primary key,
+  password        varchar(255),
+  first_name      varchar(255),
+  last_name       varchar(255),
+  email           varchar(255),
+  phone           varchar(13),
+  national_number varchar(10),
+  special_till    date,
+  score           int default 0,
+  CONSTRAINT CHK_Password CHECK ( LENGTH(password) >= 8 AND  password LIKE '%[0-9]%' AND password LIKE '%[A-Z]%' AND password LIKE '%[a-z]%')
 );
 
-CREATE UNIQUE INDEX uidx_email ON users(email);
-
-# CREATE TRIGGER create_wallet
-#     AFTER INSERT
-#     ON users
-#     FOR EACH ROW
-#     INSERT INTO wallet (username) VALUES (users.username);
+CREATE UNIQUE INDEX IF NOT EXISTS uidx_email ON users(email);
