f7ae49512e9b36cb257755bb3ffc8f0409f01a53
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 12:12:31 +0430

    feat: Add Score into Film Response

--- response/film.go
+++ response/film.go
@@ -1,13 +1,14 @@
 package response
 
 type Film struct {
-	ID             int      `json:"id"`
-	File           string   `json:"file"`
-	Name           string   `json:"name"`
-	Producers      string `json:"producers"`
-	ProductionYear int      `json:"production_year"`
-	Explanation    string   `json:"explanation"`
-	View           int      `json:"view"`
-	Price          int      `json:"price"`
-	Tags           string `json:"tags"`
+	ID             int     `json:"id"`
+	File           string  `json:"file"`
+	Name           string  `json:"name"`
+	Producers      string  `json:"producers"`
+	ProductionYear int     `json:"production_year"`
+	Explanation    string  `json:"explanation"`
+	View           int     `json:"view"`
+	Price          int     `json:"price"`
+	Tags           string  `json:"tags"`
+	Score          float64 `json:"score"`
 }
