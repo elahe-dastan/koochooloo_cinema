0d68e780f6006beceeacf989649fb35c123f19f2
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 06:57:22 +0430

    feat: Add Transaction for view

--- migration/20210710191618_watch.up.sql
+++ migration/20210710191618_watch.up.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS watch
     FOREIGN KEY (username) REFERENCES users (username)
 )
 
+
+CREATE OR REPLACE FUNCTION pay_for_watch() RETURNS trigger as
+$$
+  BEGIN
+    IF (select count(*) from user,film where film.id = NEW.film AND user.credit >= film.price AND username = NEW.username) THEN
+      UPDATE film SET view = view + 1 WHERE id = NEW.film;
+      UPDATE user SET credit = credit - ( SELECT price FROM film WHERE id = NEW.film ) WHERE username = NEW.username;
+      RETURN NEW;
+    END IF;
+      RETURN NULL;
+  END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER watch_create BEFORE INSERT on watch FOR EACH ROW EXECUTE PROCEDURE pay_for_watch();
