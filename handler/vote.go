ef01c6ebb61d7ba71797a7425dc38958b4db1163
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 07:05:46 +0430

    feat: Update Vote Handler

--- handler/vote.go
+++ handler/vote.go
@@ -15,6 +15,7 @@ type Vote struct {
 	Store *sql.DB
 }
 
+// nolint: wrapcheck
 func (v *Vote) Create(c echo.Context) error {
 	username := c.Param("username")
 	film := c.Param("film")
@@ -24,67 +25,49 @@ func (v *Vote) Create(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	//todo what if update
-	filmId, err := strconv.Atoi(film)
+	filmID, err := strconv.Atoi(film)
 	if err != nil {
-		return err
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	tx, err := v.Store.Begin()
-	if err != nil {
-		return err
-	}
+	query := fmt.Sprintf("INSERT INTO vote VALUES ('%s', %d, %d, '%s')", username, filmID, vote.Score, vote.Comment)
 
-	query := fmt.Sprintf("SELECT * FROM watch WHERE username = '%s' AND film = %d", username, filmId)
 	result, err := v.Store.Exec(query)
 	if err != nil {
-		tx.Rollback()
-		return err
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		tx.Rollback()
-		return err
-	}
-	if rowsAffected == 0 {
-		return c.JSON(http.StatusForbidden, "first watch the movie")
+	if n, _ := result.RowsAffected(); n != 1 {
+		return echo.NewHTTPError(http.StatusBadRequest, "you must watch the film before voting")
 	}
 
-	query = fmt.Sprintf("INSERT INTO vote VALUES ('%s', %d, %d, '%s')", username, filmId, vote.Score, vote.Comment)
-	if _, err = v.Store.Exec(query); err != nil {
-		tx.Rollback()
-		return err
-	}
-
-	tx.Commit()
-	// todo return object
-	return c.NoContent(http.StatusOK)
+	return c.NoContent(http.StatusCreated)
 }
 
+// nolint: wrapcheck
 func (v *Vote) Retrieve(c echo.Context) error {
-	film := c.QueryParam("film")
+	film := c.Param("film")
 
 	var votes request.Vote
 
-	// todo user is keyword?
-	//todo what if update
 	rows, err := v.Store.Query("SELECT * FROM vote WHERE film = ?", film)
 	if err != nil {
-		return err
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
+
+	if rows.Err() != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, rows.Err().Error())
+	}
+	defer rows.Close()
 
 	if err = rows.Scan(votes); err != nil {
 		return err
 	}
 
-	// todo return object
 	return c.JSON(http.StatusOK, votes)
 }
 
 func (v *Vote) Register(g *echo.Group) {
 	g.POST("/comment/:username/:film", v.Create)
-	//g.POST("/signup", f.Create)
-	//g.POST("/edit", f.Update)
-	//g.GET("/count/:key", h.Count)
+	g.GET("/comment/:film", v.Retrieve)
 }
