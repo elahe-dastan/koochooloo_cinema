989b0c84f9e751b699a15f586d8f51ce929015c8
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 13:25:44 +0430

    feat: Add Introducer Trigger

--- migration/20210712131928_introducer.up.sql
+++ migration/20210712131928_introducer.up.sql
@@ -4,4 +4,14 @@ CREATE TABLE IF NOT EXISTS introducer
     introducer varchar(255),
     FOREIGN KEY (username) REFERENCES users (username),
     FOREIGN KEY (introducer) REFERENCES users (username)
-)
+);
+
+CREATE OR REPLACE FUNCTION increase_score() RETURNS trigger as
+$$
+  BEGIN
+    UPDATE users SET score = score + 1 WHERE username = NEW.introducer;
+  END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER introducer_create BEFORE INSERT on introducer FOR EACH ROW EXECUTE PROCEDURE increase_score();
