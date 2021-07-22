b29ae03f352ba5a102313782a5bdb9392bd7771b
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 07:21:02 +0430

    feat: Improve migrations

--- migration/20210710191618_watch.up.sql
+++ migration/20210710191618_watch.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS watch
 (
     film int,
     username varchar(255),
-    number int,
+    number int default 0,
     PRIMARY KEY (film, username),
     FOREIGN KEY (film) REFERENCES film (id),
     FOREIGN KEY (username) REFERENCES users (username)
