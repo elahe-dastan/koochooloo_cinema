121b424c0d17eecb95a3a5dcb5500b4cf7d5a775
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 10:17:53 +0430

    fix: Correct Password Check Constraint

--- migration/20210704220245_user.up.sql
+++ migration/20210704220245_user.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
   username        varchar(255) primary key,
-  password        varchar(255),
+  password        varchar(255) check ( LENGTH(password) >= 8 and (password similar to '%[0-9A-Za-z]%') ),
   first_name      varchar(255),
   last_name       varchar(255),
   email           varchar(255),
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS users
   national_number varchar(10),
   special_till    date,
   score           int default 0
-  /*CONSTRAINT CHK_Password CHECK ( (LENGTH(password) >= 8) AND (password LIKE '%[0-9]%') AND (password LIKE '%[A-Z]%') AND (password LIKE '%[a-z]%') )*/
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS uidx_email ON users(email);
