b606b3a70e4fad9739551e87f8e6d4c9fe8154cb
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 07:14:11 +0430

    feat: Switch to bulk insert

--- handler/favorite.go
+++ handler/favorite.go
@@ -5,35 +5,45 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
-
 	"github.com/elahe-dastan/koochooloo_cinema/request"
+	"github.com/labstack/echo/v4"
 )
 
 type Favorite struct {
 	Store *sql.DB
 }
 
+// nolint: wrapcheck
 func (f *Favorite) Create(c echo.Context) error {
 	var body request.Favorite
-	err := c.Bind(&body)
-	if err != nil {
-		return err
+	if err := c.Bind(&body); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	for _, film := range body.Film {
-		query := fmt.Sprintf("INSERT INTO favorite VALUES ('%s', %d, '%s')", body.Username, film, body.Album)
-		if _, err = f.Store.Exec(query); err != nil {
-			return err
+	query := "INSERT INTO favorite VALUES"
+
+	for i, film := range body.Film {
+		if i != 0 {
+			query = fmt.Sprintf("%s, ('%s', %d, '%s')", query, body.Username, film, body.Album)
+		} else {
+			query = fmt.Sprintf("%s ('%s', %d, '%s')", query, body.Username, film, body.Album)
 		}
 	}
 
-	return c.NoContent(http.StatusOK)
+	query += ";"
+
+	result, err := f.Store.Exec(query)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	if n, _ := result.RowsAffected(); n != 1 {
+		return echo.NewHTTPError(http.StatusBadRequest, "you must be a special user to have a list")
+	}
+
+	return c.NoContent(http.StatusCreated)
 }
 
 func (f *Favorite) Register(g *echo.Group) {
 	g.POST("/favorite", f.Create)
-	//g.POST("/signup", f.Create)
-	//g.POST("/edit", f.Update)
-	//g.GET("/count/:key", h.Count)
 }
