052bf3d46ee2f99bf9537c990a93cf3d8c1bc608
Elahe Dastan <elahe.dstn@gmail.com>
2021-07-22 07:18:23 +0430

    feat: Improve db package

--- db/db.go
+++ db/db.go
@@ -3,12 +3,10 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"time"
+	"log"
 
 	_ "github.com/lib/pq"
 )
-
-const connectionTimeout = 10 * time.Second
 
 const (
 	host     = "localhost"
@@ -20,20 +18,20 @@ const (
 
 // New creates a new postgres connection and tests it.
 func New() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	url := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+
+	db, err := sql.Open("postgres", url)
 	if err != nil {
 		panic(err)
 	}
 
-	err = db.Ping()
-	if err != nil {
+	if err = db.Ping(); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Successfully connected!")
+	log.Println("Successfully connected!")
 
 	return db, nil
 }
